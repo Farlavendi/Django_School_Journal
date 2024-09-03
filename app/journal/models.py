9e5870f5934b81d68d9b3057362214facb635cde
DenisBichuk <denisbichuk@gmail.com>
2024-09-03 07:52:15 +0300

    Added verbose_name and verbose_name_plural fields to models. Added translation for models' fields with gettext_lazy. Added indexes to models.

--- app/journal/models.py
+++ app/journal/models.py
@@ -1,21 +1,63 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Class(models.Model):
-    number = models.IntegerField()
+    number = models.IntegerField(
+        verbose_name=_('Class number')
+    )
+
+    class Meta:
+        indexes = (
+            models.Index(fields=['number']),
+        )
+        verbose_name = _('Class')
+        verbose_name_plural = _('Classes')
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    _class = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='students')
+    name = models.CharField(
+        verbose_name=_("Student's name"),
+        max_length=100
+    )
+    _class = models.ForeignKey(
+        verbose_name=_('Class'),
+        to=Class,
+        on_delete=models.CASCADE,
+        related_name=_('students')
+    )
 
-    def __str__(self):
+    class Meta:
+        indexes = (
+            models.Index(fields=('name',)),
+            models.Index(fields=('_class',))
+        )
+        verbose_name = _('Student')
+        verbose_name_plural = _('Students')
+
+    def __str__(self) -> str:
         return f'{self.name} is in {self._class} class.'
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=100)
-    _class = models.OneToOneField(Class, on_delete=models.CASCADE)
+    name = models.CharField(
+        verbose_name=_("Teacher's name"),
+        max_length=100
+    )
+    _class = models.OneToOneField(
+        verbose_name=_('Class'),
+        to=Class,
+        on_delete=models.CASCADE
+    )
 
-    def __str__(self):
+    class Meta:
+        class Meta:
+            indexes = (
+                models.Index(fields=('name',)),
+                models.Index(fields=('_class',))
+            )
+        verbose_name = _('Teacher')
+        verbose_name_plural = _('Teachers')
+
+    def __str__(self) -> str:
         return f'{self.name} is a teacher in {self._class} class.'
