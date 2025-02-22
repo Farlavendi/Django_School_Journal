2bbad3b0f82386ec084fdee128fd2b90c744282e
Denis Bichuk <denisbicuk18@gmail.com>
2025-02-22 19:01:21 +0200

    Moved from psycopg2 to psycopg.

--- src/core/settings.py
+++ src/core/settings.py
@@ -65,12 +65,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": "db",  # Docker container name
-        # "HOST": "localhost",  # for local startup
+        # "HOST": "db",  # Docker container name
+        "HOST": "localhost",  # for local startup
         "PORT": os.getenv("DB_PORT"),
     }
 }
